25597aa5471cc12908a6dd7515291c838da1f292
Emm Ajayi <truevirtue001@yahoo.com>
2024-09-05 21:46:05 +0100

    Installed Radix UI icons and add nav links

--- app/routes/_guestlayout.tsx
+++ app/routes/_guestlayout.tsx
@@ -1,10 +1,10 @@
-import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+import { AppShell, Burger, Group, Skeleton, NavLink } from '@mantine/core';
+import { HomeIcon, DropdownMenuIcon, BackpackIcon } from '@radix-ui/react-icons';
 import { useDisclosure } from '@mantine/hooks';
 // import { MantineLogo } from '@mantinex/mantine-logo';
 import { Outlet } from "@remix-run/react";
-import { Link } from "@remix-run/react";
 
-export default function GuestLayout() {
+export default function UserLayout() {
   const [opened, { toggle }] = useDisclosure();
 
   return (
@@ -20,12 +20,33 @@ export default function GuestLayout() {
         </Group>
       </AppShell.Header>
       <AppShell.Navbar p="md">
-        Navbar
-        {Array(15)
-          .fill(0)
-          .map((_, index) => (
-            <Skeleton key={index} h={28} mt="sm" animate={false} />
-          ))}
+      <NavLink label="First child link" href="#required-for-focus" leftSection={<HomeIcon />} />
+      <NavLink
+          href="#required-for-focus"
+          label="First parent link"
+          leftSection={<DropdownMenuIcon />}
+          childrenOffset={28}
+        >
+          <NavLink href="#required-for-focus" label="First child link" />
+          <NavLink label="Second child link" href="#required-for-focus" />
+          <NavLink label="Nested parent link" childrenOffset={28} href="#required-for-focus">
+            <NavLink label="First child link" href="#required-for-focus" />
+            <NavLink label="Second child link" href="#required-for-focus" />
+            <NavLink label="Third child link" href="#required-for-focus" />
+          </NavLink>
+        </NavLink>
+
+        <NavLink
+          href="#required-for-focus"
+          label="Second parent link"
+          leftSection={<BackpackIcon />}
+          childrenOffset={28}
+          defaultOpened
+        >
+          <NavLink label="First child link" href="#required-for-focus" />
+          <NavLink label="Second child link" href="#required-for-focus" />
+          <NavLink label="Third child link" href="#required-for-focus" />
+        </NavLink>
       </AppShell.Navbar>
       <AppShell.Main>
         <Outlet />
